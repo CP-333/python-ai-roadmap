1a93ab6f6e03bf1bd9e0073be841484ef3a3a410
Guo <1115166303@qq.com>
2026-04-22 02:33:42 +0800

    day1 upgraded with except

--- 01_python_basics/day1_intro.py
+++ 01_python_basics/day1_intro.py
@@ -1,5 +1,8 @@
 name = input("请输入你的名字：")
-age = int(input("请输入你的年龄："))
+try:
+    age = int(input("请输入你的年龄："))
+except:
+    print("请输入正确的数字")
 goal = input("请输入你的目标：")
 
 print(f"你好，我叫{name}, 今年{age}岁, 我想学{goal}")
